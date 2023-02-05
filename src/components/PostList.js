3e9753bb63018c91f20157c72a7ed962010651c1
kazım <kazimnergiz123@gmail.com>
2023-02-06 00:06:14 +0300

    S10Challenge esnek gorev ile birlikte tamamlanmıştır

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,8 +1,16 @@
 import React from "react";
 import Post from "./Post";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { verileriSil } from "../actions";
 
 const PostList = () => {
-  const notlar = [];
+  const dispatch = useDispatch();
+  const notlar = useSelector((depo) => depo.notlar);
+
+  useEffect(() => {
+    localStorage.setItem("s10ch", JSON.stringify(notlar));
+  }, [notlar]);
 
   return notlar.length === 0 ? (
     <div className="beyazKutu text-center p-6">Hiç notunuz yok</div>
@@ -11,6 +19,12 @@ const PostList = () => {
       {notlar.map((not) => (
         <Post item={not} key={not.id} />
       ))}
+      <button
+        className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded transition duration-150 ease-in-out"
+        onClick={() => dispatch(verileriSil())}
+      >
+        Tüm Listeyi Temizle
+      </button>
     </div>
   );
 };
